00763519688e58f4216843470807cfb06c34bd2a
iVoid1 <yousef.a.a.mutawa@gmail.com>
2025-04-06 16:56:02 +0300

    Refactor KeyReader: improve type hints, handle asynchronous key reading, and ensure proper event handling

--- python/shortcutmanger/key_reader.py
+++ python/shortcutmanger/key_reader.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from keyboard import KeyboardEvent
 import keyboard
 from pynput.keyboard import Key, KeyCode
@@ -15,33 +16,47 @@ class KeyReader:
         self.active_keys_str: str = ""
         self.init: bool = False
 
-    async def read_key(self, stop_key: Key | KeyCode | str = "esc", func=None):
+    async def read_key(self, stop_key: Key | KeyCode | str = "esc", func: Callable[[], None] | None = None):
         """Reads keys asynchronously until stop_key is pressed."""
+
         key = self.get_key(stop_key)
-        loop = asyncio.get_running_loop()
+        if key is None:
+            return
+        
+        # Initialize if not already done
+        if not self.init:
+            self.clear()
+            self.init = True
 
         # Hook into all keyboard events
         keyboard.hook(lambda event: self.on_key_event(event, func))
         try:
             while self.last_key != key:
                 await asyncio.sleep(0.1)  # Allow event loop to run
-        except :
+                
+        except:
             pass
+
+        finally:
+            keyboard.unhook_all()
+
         print("Stopping key reader...")
 
-    def on_key_event(self, key_event: KeyboardEvent, func):
+    def on_key_event(self, key_event: KeyboardEvent, func: Callable[[], None] | None):
         """Handles key events."""
         if key_event.name is None:
             return
 
-        if func:
-            func()  # Call the function safely if provided
+        if func is not None:
+            func()  # Call the provided function on key event        
 
         if key_event.event_type == "down":  # When a key is pressed
             self.last_key = key_event.name  # Store the key pressed
             self.pressed.append(key_event.name)  # Track all pressed keys
+
             if key_event.name not in self.active_keys:
                 self.active_keys.append(key_event.name)  # Add to active keys
+
             self.active_keys_str += key_event.name
 
         elif key_event.event_type == "up" and key_event.name in self.active_keys:
